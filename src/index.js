65da939df1bb4d56574075eba5f862d59e2abea2
Alishah Safdari <alishahsafdari@gmail.com>
2023-05-03 22:03:19 +0430

    The Array is added in index.js file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import './style.css';
-import Selector from '../modules/selectors.js';
-import Todo from '../modules/todo.js';
 
-const objectSelector = new Selector();
-const objectTodo = new Todo();
+const todoBody = document.querySelector('.js-todo-body');
 
-window.addEventListener('load', () => {
-  objectTodo.handleWindowLoad();
-});
-objectSelector.todoForm.addEventListener('submit', () => {
-  objectTodo.hanleFormSubmit();
-});
+const todos = [
+  {
+    id: 1,
+    task: 'go shopping and buy new things',
+    status: true,
+  },
+  {
+    id: 2,
+    task: 'read book',
+    status: false,
+  },
+  {
+    id: 3,
+    task: 'start new job',
+    status: true,
+  },
+];
+
+const handleWindowLoad = () => {
+  if (todos.length > 0) {
+    todos.forEach((todo) => {
+      const todoList = `<li data-id="${todo.id}" data-status="${todo.status}">
+      <label for="${todo.id}">
+      <input type="checkbox" id="${todo.id}" value="${todo.id}"
+      ${todo.status === 'complete' ? 'true' : false}/>
+      <input type="text" value="${todo.task}" readonly />
+      </label>
+          <div class="action">
+          <button class="js-delete">
+              <i class="ri-delete-bin-fill"></i>
+          </button>
+          </div> 
+      </li>`;
+      todoBody.querySelector('.js-todo-list').innerHTML += todoList;
+    });
+  }
+};
+
+handleWindowLoad();
