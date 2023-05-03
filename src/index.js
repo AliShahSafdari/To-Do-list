9faba73abe706debf3afe72f7add32da23b896e1
Yordi <yorditjordin@gmail.com>
2023-05-04 00:12:28 +0300

    in this repo t tried to understand how to set event listeners while rendering works but it still have limitation

--- src/index.js
+++ src/index.js
@@ -27,21 +27,40 @@ const todos = [
 
 const handleWindowLoad = () => {
   if (todos.length > 0) {
-    todos.forEach((todo) => {
-      const todoList = `<li data-id="${todo.id}" data-status="${todo.status}">
-      <label for="${todo.id}">
-      <input type="checkbox" id="${todo.id}" value="${todo.id}"
-      ${todo.status === 'complete' ? 'true' : false}/>
-      <input type="text" value="${todo.task}" readonly />
+      for(let i=0; i<todos.length; i++){
+   // todos.forEach((todo) => { had to remove this as it wasn't setting event listeners for all delete and checkbox ..
+      const todoList = `<li data-id="${todos[i].id}" data-status="${todos[i].status}">
+      <label>
+      <input type="checkbox" id="${todos[i].id}" value="${todos[i].id}"
+      ${todos[i].status === 'complete' ? 'true' : false}/>
+      <input type="text" value="${todos[i].task}" readonly />
       </label>
-          <div class="action">
-          <button class="js-delete">
-              <i class="ri-delete-bin-fill"></i>
-          </button>
-          </div> 
+
+      <div class="action">
+        <button class="js-delete"  id="trash-${todos[i].id}">
+          <i class="ri-delete-bin-fill"></i>
+        </button>
+      </div> 
+      
       </li>`;
       todoBody.querySelector('.js-todo-list').innerHTML += todoList;
-    });
+      // add evemt listener on the checkbox and delete 
+      const li = todoBody.firstElementChild.children[i];
+      const deleteElement = li.lastElementChild.firstElementChild;
+      const checkboxElement = li.firstElementChild.firstElementChild;
+      deleteElement.addEventListener('click', ()=>{
+        alert('Just clicked delete no - '+ todos[i].id)
+      })
+      checkboxElement.addEventListener('change', (e)=>{
+        alert('Just checked checkmark  number - ' +todos[i].id+ '  -'+ e.target.checked )
+      })
+ 
+      console.log('li- ',li, deleteElement, checkboxElement);
+  //  });
+     }   
+  }
+  else{
+    todoBody.querySelector('.js-todo-list').innerHTML = `<p> list is empty </p>`;
   }
 };
 
